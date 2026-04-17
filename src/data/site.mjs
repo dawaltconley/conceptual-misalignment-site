b8e391c4c9308bb6a3f3b70ab03ff462ec8681fd
Dylan Awalt-Conley <web@dylan.ac>
2026-04-17 14:10:06 -0400

    update copyright year

--- src/data/site.mjs
+++ src/data/site.mjs
@@ -5,6 +5,7 @@
  * @property {string} SiteConfig.title
  * @property {string} SiteConfig.description
  * @property {URL} SiteConfig.domain
+ * @property {Date} SiteConfig.copyright
  * @property {string} [SiteConfig.ogImage]
  * @property {string} [SiteConfig.favicon]
  */
@@ -14,6 +15,7 @@ export const SITE = {
   title: 'Conceptual Misalignment',
   description: 'A network graph representation of conceptual misalignment.',
   domain: new URL('https://conceptual-misalignment.netlify.app/'),
+  copyright: new Date('2026-04-17'),
   ogImage: undefined,
   // favicon: '/favicon.svg',
 }
