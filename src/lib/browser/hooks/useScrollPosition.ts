fbfd6c26673d9ef0a56f1cef89510834beac8d60
Dylan Awalt-Conley <web@dylan.ac>
2026-08-11 14:32:08 -0400

    recalculate scroll position on resize

--- src/lib/browser/hooks/useScrollPosition.ts
+++ src/lib/browser/hooks/useScrollPosition.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import { useResizeObserver } from 'use-resize-observer'
 
 export type ScrollPosition = 'top' | 'middle' | 'bottom' | null
 
@@ -12,26 +13,41 @@ export default function useScrollPosition<T extends Element>({
   const [position, setPosition] = useState<ScrollPosition>(null)
   const ref = useRef<T>(null)
 
+  useResizeObserver<T>({
+    ref,
+    onResize: ({ height }) => {
+      const el = ref.current
+      if (!el || !height) return
+      console.log('resize', getScrollPosition(el, threshold))
+      setPosition(() => getScrollPosition(el, threshold))
+    },
+  })
+
   useEffect(() => {
     const e = ref.current
     if (!e) return
     const scrollListener = () => {
-      setPosition(() => {
-        if (e.scrollTop <= threshold) {
-          return 'top'
-        } else if (e.scrollHeight - e.scrollTop - e.clientHeight <= threshold) {
-          return 'bottom'
-        } else {
-          return 'middle'
-        }
-      })
+      setPosition(() => getScrollPosition(e, threshold))
     }
     scrollListener() // fire once to capture initial state
     e.addEventListener('scroll', scrollListener, { passive: true })
     return () => {
       e.removeEventListener('scroll', scrollListener)
     }
-  }, [])
+  }, [threshold])
 
   return { ref, position }
+}
+
+function getScrollPosition(e: Element, threshold = 0): ScrollPosition {
+  if (e.scrollHeight <= e.clientHeight) {
+    return null
+  }
+  if (e.scrollTop <= threshold) {
+    return 'top'
+  } else if (e.scrollHeight - e.scrollTop - e.clientHeight <= threshold) {
+    return 'bottom'
+  } else {
+    return 'middle'
+  }
 }
